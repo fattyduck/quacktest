4b6eb58ea13ae479b8001d1dd3de2d0817e56334
fattyduck <dison.ruan@yahoo.com>
2015-03-01 17:42:21 -0500

    Awesome

---
+++ src/nyc/c4q/disonruan/Main.java
@@ -0,0 +1,21 @@
+package nyc.c4q.disonruan;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("+------------------------------------+");
+        System.out.println("|                                 ## |");
+        System.out.println("|                                 ## |");
+        System.out.println("|                    Dison Ruan      |");
+        System.out.println("|                    1990 pig ave    |");
+        System.out.println("|                    Troll City, CN  |");
+        System.out.println("+------------------------------------+");
+        /**adsf
+        *jkl;
+        * 1234
+        * 78910
+        * 2164
+        * */
+    }
+}
---
+++ src/nyc/c4q/disonruan/testclass.java
@@ -0,0 +1,31 @@
+package nyc.c4q.disonruan;
+
+/**
+ * Created by fattyduck on 3/1/15.
+ */
+public class testclass {
+
+    public static void main(String[] args) {
+        // write your code here
+        int cow=200;
+        double pig=1.5;
+        double duck=pig*cow;
+        System.out.println(duck);
+        System.out.println("I said \"I am testing cows\" \n\n p.s. \"I hate you\""+" X"+cow + "\n");
+        System.out.println ("System.out.println(\"I said \"I am testing cows p.s. \"I hate you x200\");" );
+
+        System.out.println("\n");
+        System.out.println("DDD   RRR");
+        System.out.println("D  D  R  R");
+        System.out.println("D  D  R R ");
+        System.out.println("D  D  R");
+        System.out.println("D  D  R R");
+        System.out.println("DDD   R  R \n\n");
+
+        int f=-4;
+        int c=f*9/5+32;
+        System.out.println("Temperature: " + c + " F");
+        System.out.println("Temperature: " + (-4*9/5+32) + " F");
+    }
+}
+
